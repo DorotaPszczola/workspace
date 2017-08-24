48be7ca51c5f775f8f8af0ea8fc170e0f0a4866b
Dorota-Komputer\Dorota <d.a.pszczola@gmail.com>
2017-08-24 14:08:07 +0200

    triangle area added do js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 // scripts.js
 
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+var a = prompt('Enter a length');
+var h = prompt('Enter h length');
+var triangleArea = a*h/2;
+
+alert('This triangle area equals ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
