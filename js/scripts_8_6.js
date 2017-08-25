09f9619f7884f3d6954bcc44559c96dc7503213a
Dorota-Komputer\Dorota <d.a.pszczola@gmail.com>
2017-08-25 15:25:32 +0200

    added else if

--- js/scripts_8_6.js
+++ js/scripts_8_6.js
@@ -3,5 +3,9 @@
 var a = prompt('Enter value for a');
 var b = prompt('Enter value for b');
 var value = ((a*a)+(2*a*b)-(b*b));
-(value > 0) ? alert('The final value is ' + value + ' and the result is positive') : alert('The final value is ' + value + ' and the result is negative');
-(value = 0) ? alert('The final value is equal to 0.') : alert('The final value is different from 0.');
+if (value > 0) {
+	(alert('The final value is ' + value + ' and the result is positive'))
+	} else if  (value < 0 ) { 
+		(alert('The final value is ' + value + ' and the result is negative'))
+	} else { 
+		(alert('The final value is equal to 0.')) }
