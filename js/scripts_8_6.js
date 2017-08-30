6ebb37b7113ecadd34411aa7e00dd7a7bff2dee6
Dorota-Komputer\Dorota <d.a.pszczola@gmail.com>
2017-08-30 13:59:15 +0200

    better syntax

--- js/scripts_8_6.js
+++ js/scripts_8_6.js
@@ -4,8 +4,8 @@ var a = prompt('Enter value for a');
 var b = prompt('Enter value for b');
 var value = ((a*a)+(2*a*b)-(b*b));
 if (value > 0) {
-	(alert('The final value is ' + value + ' and the result is positive'))
+	alert('The final value is ' + value + ' and the result is positive')
 	} else if  (value < 0 ) { 
-		(alert('The final value is ' + value + ' and the result is negative'))
+		alert('The final value is ' + value + ' and the result is negative')
 	} else { 
-		(alert('The final value is equal to 0.')) }
+		alert('The final value is equal to 0.') }
