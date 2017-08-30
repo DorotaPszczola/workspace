61948d2d2cea1df7dce19ba6456dad5502a825a4
Dorota-Komputer\Dorota <d.a.pszczola@gmail.com>
2017-08-30 15:48:27 +0200

    corrected condition

--- js/scripts_9_1.js
+++ js/scripts_9_1.js
@@ -1,14 +1,19 @@
 // project 9.1
 
 function getTriangleArea(a, h) {
-	if (a <= 0) {
-		alert('Wrong number. "a" must be bigger than 0')
-	} 
-	if (h <= 0) {
-		alert('Wrong number. "h" must be bigger than 0')
-	} 
-	return (a*h/2)
+	if (a > 0 && h > 0) {
+		return (a*h/2)
+	} else {
+		alert("Nieprawidłowe dane.");
+		return false;
+	}
 }
-console.log( getTriangleArea(10,6) )
 
-	var triangle1Area = getTriangleArea(10, 15);
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(-10, 5);
+var triangle3Area = getTriangleArea(7, 8);
+
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
+
