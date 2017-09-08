db7c033cfc62433495d72a48d418a07a3172145d
Dorota-Komputer\Dorota <d.a.pszczola@gmail.com>
2017-09-08 14:12:28 +0200

    new project 9.3

---
+++ js/scripts_9_3.js
@@ -0,0 +1,9 @@
+// project 9.3
+
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
+var dinosaur = 'triceratops';
+var dinosaur = dinosaur.toUpperCase();
+var text = text.replace('Velociraptor', dinosaur);
+console.log(text.substr(0, text.length/2));
+
+
