32d9cfbe0f2d3746902c922155cb6ccdb24cfee1
Dorota-Komputer\Dorota <d.a.pszczola@gmail.com>
2017-09-08 16:06:10 +0200

    symetrical tree - added spaces before stars

--- js/scripts_9_4.js
+++ js/scripts_9_4.js
@@ -1,13 +1,27 @@
 // project 9.4
 
 function drawTree(h) {
+	
 	for (var i = 1 ; i <= h ; i++) {
 		var star = "";
+		
 		for (var j = 0 ; j <= i ; j++ ) {
-			star += '*';
-			console.log(star); 
+			var g = h - j
+			space = "";
+
+			for (var k = g ; g > 0 ; g-- ) { 
+				space += ' ';
+				
+			}
+
+			star += '**';
+			var tree = space.concat(star);
+			console.log(tree); 
+
 		}
 	}
 }
 
 drawTree(5)
+
+
